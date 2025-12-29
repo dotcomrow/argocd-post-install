b7c3f77c9dedb1c40d0b65200fd308031c705438
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-12-29 11:58:35 -0500

    removing keys
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- generate-keyset.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# This script generates a keyset for the given keyset name and keyset type.
-# It generates a private key and a public key, and then encodes the private key in base64 format.
-ssh-keygen -t ed25519 -C "administrator@suncoast.systems" -f ./fleet_pk -N "" -q
